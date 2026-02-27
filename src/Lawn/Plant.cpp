b014771970d663d4ba0643838ecf5e7cc6ba8e5f
Zhou Qiankang <wszqkzqk@qq.com>
2026-02-27 18:02:14 +0800

    Fix: Correct the projectile spawn position for Leftpeater
    The initial X-coordinate for the Leftpeater's (reverse-facing Repeater) projectile was incorrectly set to `mX + aOffsetX + 27`, causing the peas to spawn from the right side of the plant instead of the left.
    
    This commit updates the offset to `-57` to match the correct spawn position for the left-facing head.
    
    Reference for the exact data: https://wiki.pvz1.com/doku.php?id=%E6%8A%80%E6%9C%AF:%E7%B2%BE%E7%A1%AE%E6%95%B0%E6%8D%AE
    
    Close #77
    
    Signed-off-by: Zhou Qiankang <wszqkzqk@qq.com>

--- src/Lawn/Plant.cpp
+++ src/Lawn/Plant.cpp
@@ -4665,7 +4665,7 @@ void Plant::Fire(Zombie* theTargetZombie, int theRow, PlantWeapon thePlantWeapon
     {
         int aOffsetX, aOffsetY;
         GetPeaHeadOffset(aOffsetX, aOffsetY);
-        aOriginX = mX + aOffsetX + 27;
+        aOriginX = mX + aOffsetX - 57;
         aOriginY = mY + aOffsetY - 33;
     }
     else if (mSeedType == SeedType::SEED_GATLINGPEA)
